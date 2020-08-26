e864137b58e2e35547232d2ceab1e6abcf5b0bea
billette <billette@wcentric.com>
2020-08-26 17:35:48 +0200

    add go programming blueprints

---
+++ go-programming-blueprints/chat/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+	"text/template"
+	"path/filepath"
+)
+
+type templateHandler struct {
+	once		sync.Once
+	filename	string
+	templ		*template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<head>
+					<title>Chat</title>
+				</head>
+				<body>
+					Let's chat!
+				</body>
+			</html>
+		`))
+	})
+
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
