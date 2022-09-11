20d704f37b063e610f18b9718b049f5aaf25f035
benjix <benjamin.billette@hotmail.fr>
2022-09-11 16:24:41 +0200

    add portsf.c to sfpan.cpp

--- the-audio-programming-book/src/sfpan/main.cpp
+++ the-audio-programming-book/src/sfpan/main.cpp
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 
 #include "breakpoints.h"
+#include "portsf.h"
+
+#define NFRAMES 100
+
+/* panning function returns a PANPOS object */
+PANPOS simplepan(double position);
+/* replacement for simplepan: constant power panning function */
+PANPOS constpower(double position);
 
 enum {ARG_PROGNAME, ARG_INFILE, ARG_OUTFILE, ARG_BRKFILE, ARG_NARGS};
 
@@ -16,11 +24,16 @@ int main(int argc, char**argv)
 
     long size;
 
+    double timeincr, sampletime;
+
     float* inframe = NULL;
 	float* outframe = NULL;
 
-    FILE* fp = NULL;
-    BREAKPOINT* points = NULL;
+	BREAKPOINT* points = NULL;
+	FILE* fp = NULL;
+	PANPOS pos;
+	PSF_PROPS inprops, outprops;
+	psf_format outformat = PSF_FMT_UNKNOWN;
 
     if (argc != ARG_NARGS) {
         printf(
@@ -37,11 +50,7 @@ int main(int argc, char**argv)
     /* read breakpoint file and verify it */
     fp = fopen(argv[ARG_BRKFILE], "r");
     if (fp == NULL) {
-        printf(
-            "Error: unable to open"
-            "breakpoint file %s\n",
-            argv[ARG_BRKFILE]
-        );
+        printf("Error: unable to open breakpoint file %s\n", argv[ARG_BRKFILE]);
         error++;
         goto exit;
     }
@@ -68,15 +77,61 @@ int main(int argc, char**argv)
         goto exit;
     }
 
+    /* check if breakpoint values are in range */
     if (!inrange(points, -1, 1.0, size)) {
-        printf("Error in breakpoint file: "
-            "values out of range -1 to +1 \n");
+        printf("Error in breakpoint file: values out of range -1 to +1 \n");
         error++;
         goto exit;
     }
 
-    // TODO: all the stuffs with portsf to open a file
-    // use that: https://github.com/umnum/Audio_Programs/blob/master/CH2/sfpan/sfpan.c
+    /* start up portsf */
+    if (psf_init()) {
+        printf("Error: unable to start portsf.\n");
+        return 1;
+    }
+
+    /* open infile */
+    ifd = psf_sndOpen(argv[ARG_INFILE], &inprops, 0);
+    if (ifd < 0) {
+        printf("Error: unable to open \"%s\"\n", argv[ARG_INFILE]);
+        return 1;
+    }
+
+    /* check if infile is 8-bit */
+	if (inprops.samptype == PSF_SAMP_8) {
+		printf("ERROR: portsf does not support 8-bit soundfiles.\n");
+		error++;
+		goto exit;
+	}
+
+	/* check if infile is in mono */
+	if (inprops.chans != 1) {
+		printf("ERROR: infile must be mono.\n");
+		error++;
+		goto exit;
+	}
+
+	/* properties of infile and outfile will be the same except infile is mono and outfile is stereo */
+	outprops = inprops;
+	outprops.chans = 2;
+
+	ofd = psf_sndCreate(argv[ARG_OUTFILE], &outprops, 0, 0, PSF_CREATE_RDWR);
+	if (ofd < 0) {
+        printf("Error: unable to create \"%s\"\n", argv[ARG_OUTFILE]);
+        error++;
+        goto exit;
+	}
+
+    /* check if outfile extension is one we know about */
+	outformat = psf_getFormatExt(argv[ARG_OUTFILE]);
+	if (outformat == PSF_FMT_UNKNOWN) {
+		printf("Outfile name \"%s\" has unknown format.\n"
+		       "Use any of .wav .aiff .aif .afc .aifc\n",
+		        argv[ARG_OUTFILE]
+        );
+		error++;
+		goto exit;
+	}
 
     /* allocate space for input frame buffer */
 	inframe = (float*)malloc(sizeof(float)*inprops.chans*NFRAMES);
@@ -89,7 +144,7 @@ int main(int argc, char**argv)
 
     // inframe est le buffer, nframe le nombre qu'on veut lire, i.e. : 100 ou moins
     // framesread = le nombre lu
-    while ((framesread = psf_sndReadFloatFrames(ifd, inframe, nframe)) > 0) {
+    while ((framesread = psf_sndReadFloatFrames(ifd, inframe, NFRAMES)) > 0) {
         int i, out_i;
         double stereopos;
 
@@ -98,7 +153,8 @@ int main(int argc, char**argv)
             stereopos = val_at_brktime(points, size, sampletime);
 
             // don't know what is that ???
-            pos = simplepan(stereopos);
+            /* replacement for simplepan: constant power panning function */
+            pos = constpower(stereopos);
 
             // on set sur le buffer les trucs en stéréo pour chaque valeur de i
             outframe[out_i++] = (float)(inframe[i] * pos.left);
@@ -121,17 +177,17 @@ int main(int argc, char**argv)
     exit:
 	if (fp)
 		fclose(fp);
-	// if (points)
-	//	free(points);
-	// if (ifd)
-	//	psf_sndClose(ifd);
-	// if (ofd)
-	//	psf_sndClose(ofd);
-	// if (inbuffer)
-    //    free(inbuffer);
-	// if (outbuffer)
-	//	free(outbuffer);
-	// psf_finish();
+	if (points)
+		free(points);
+	if (ifd)
+		psf_sndClose(ifd);
+	if (ofd)
+		psf_sndClose(ofd);
+	if (inframe)
+        free(inframe);
+	if (outframe)
+		free(outframe);
+	psf_finish();
 
 	return error;
 }
